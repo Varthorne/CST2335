7e3fde6f2de52c570c1abcc4428dbe61913a7d59
Varthorne <joly0055@algonquinlive.com>
2019-01-21 18:26:01 -0500

    Lab 2 commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.androidlabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.example.androidlabs.R.layout.activity_main_grid;
+import static com.example.androidlabs.R.layout.activity_main_linear;
+import static com.example.androidlabs.R.layout.activity_main_relative;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main_relative);
     }
 }

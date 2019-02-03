7e35f78f2fa9c685f7a3a9c98ae6a764b6c7b272
Varthorne <joly0055@algonquinlive.com>
2019-02-03 11:02:14 -0500

    Finished Lab 3 - Not demonstrated yet

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -7,13 +7,14 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText emailField;
-    SharedPreferences sp;
+    private ImageButton imageButton;
+    public static final String ACTIVITY_NAME = "ProfileActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,13 +22,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_layout);
 
-        emailField = findViewById(R.id.profileEdit2);
-        sp = getSharedPreferences("Lab3Prefs", Context.MODE_PRIVATE);
+        EditText emailField = findViewById(R.id.profileEdit2);
+        SharedPreferences sp = getSharedPreferences("Lab3Prefs", Context.MODE_PRIVATE);
 
         String emailString = sp.getString("userEmail", "Email");
 
         emailField.setText(emailString);
 
+        imageButton = findViewById(R.id.profilePictureButton);
+        imageButton.setOnClickListener( e -> {
+
+            dispatchTakePictureIntent();
+        });
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onCreate");
     }
 
     @Override
@@ -35,8 +43,10 @@ public class ProfileActivity extends AppCompatActivity {
         if (requestCode == RequestCode.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageButton.setImageBitmap(imageBitmap);
+            imageButton.setImageBitmap(imageBitmap);
         }
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onActivityResult");
     }
 
 
@@ -45,7 +55,42 @@ public class ProfileActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, RequestCode.REQUEST_IMAGE_CAPTURE);
         }
+
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.e(ACTIVITY_NAME, "In function:" + "onDestroy");
+    }
 
 }
